5e7ef43a3fb63f3b6b21be752b0994cd135e1915
DuyPh1472 <saophadao1472@gmail.com>
2025-06-27 17:40:28 +0700

    Implement authentication and authorization logic

--- Repos/TAs/TAs.Domain/Entities/User.cs
+++ Repos/TAs/TAs.Domain/Entities/User.cs
@@ -1,6 +1,14 @@
 using Microsoft.AspNetCore.Identity;
 namespace TAs.Domain.Entities
 {
-    public class User : IdentityUser<Guid>;
- 
+    public class User : IdentityUser<Guid>
+    {
+        public DateOnly DateOfBirth { get; set; }
+        public string Avatar { get; set; } = default!;
+        public string Region { get; set; } = default!;
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+        public Guid CreatedBy { get; set; }
+        public Guid UpdatedBy { get; set; }
+    }
 }
